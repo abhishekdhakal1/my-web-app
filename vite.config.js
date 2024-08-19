051ac05266581ea7c17a6ce816c2567786312b61
Abhishek Dhakal <abhishekdhakal1826@gmail.com>
2024-08-19 21:14:34 +0545

    Deploy

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import ghPages from 'vite-plugin-gh-pages';
+//import ghPages from 'vite-plugin-gh-pages';
 
 export default defineConfig({
-  plugins: [react(), ghPages()],
+  plugins: [react()],
   base: '/', 
 });
