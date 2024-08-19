a4784256ba91968f8fdd6147c80f686059f5e592
Abhishek Dhakal <abhishekdhakal1826@gmail.com>
2024-08-19 21:27:08 +0545

    errorfu

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-//import ghPages from 'vite-plugin-gh-pages';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', 
 });
